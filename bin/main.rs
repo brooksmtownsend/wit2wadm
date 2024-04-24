e446f3716ac8b120584ab7ac9ea4d916b9c43d4b
Brooks Townsend <brooksmtownsend@gmail.com>
2024-04-24 10:20:17 -0400

    feat(cli): add support for component or wit
    Signed-off-by: Brooks Townsend <brooksmtownsend@gmail.com>

--- bin/main.rs
+++ bin/main.rs
@@ -1,19 +1,36 @@
-use wit2wadm::convert::wit2wadm;
-use wit_parser::Resolve;
+use std::path::PathBuf;
+
+use anyhow::Context as _;
+use clap::Parser;
+use wit2wadm::convert::{wit2wadm_from_component, wit2wadm_from_folder};
+
+#[derive(Parser)]
+#[clap(version, author = "wasmCloud", arg_required_else_help = true)]
+/// wit2wasm is a tool for converting a WIT directory or a WebAssembly component into a deployable https://github.com/wasmcloud/wadm manifests.
+/// Either supply a WIT folder and a world name or a WebAssembly component and the resulting manifest will be printed to stdout.
+/// See https://github.com/brooksmtownsend/wit2wadm for more information.
+struct Args {
+    #[clap(name = "wit_folder_or_component")]
+    /// The path to a WIT folder including dependencies and a world, or a built Wasm component
+    wit_folder_or_component: PathBuf,
+    /// The world name to use to convert to a manifest, required if a WIT folder is provided
+    #[clap(name = "world_name")]
+    world_name: Option<String>,
+}
 
 fn main() {
-    let args = std::env::args().collect::<Vec<_>>();
-    let mut resolve = Resolve::new();
-    // TODO: Depending on args we could parse a component
+    let args = Args::parse();
+    let manifest = if args.wit_folder_or_component.is_dir() {
+        let world_name = args
+            .world_name
+            .expect("world name is required when providing a WIT folder");
+        wit2wadm_from_folder(&args.wit_folder_or_component, &world_name)
+    } else {
+        wit2wadm_from_component(args.wit_folder_or_component)
+    }
+    .context("failed to convert WIT to WADM")
+    .expect("should be able to convert WIT to WADM");
 
-    let wit_folder = args.get(1).expect("missing wit folder");
-    let world_name = args.get(2).expect("missing world name");
-
-    resolve
-        .push_path(wit_folder)
-        .expect("should be able to load wits");
-
-    let manifest = wit2wadm(resolve, world_name).expect("should be able to convert to manifest");
     let yaml_result = serde_yaml::to_string(&manifest);
     match yaml_result {
         Ok(yaml_string) => println!("{}", yaml_string),
--- crates/wit2wadm/src/convert.rs
+++ crates/wit2wadm/src/convert.rs
@@ -1,19 +1,63 @@
-use anyhow::Context as _;
+use std::path::Path;
+
+use anyhow::{bail, Context as _};
 use wadm::model::Manifest;
-use wit_parser::Resolve;
+use wit_parser::{Resolve, World};
 
 use crate::manifest;
 
-/// Converts a component [Resolve] and world name into a wadm application manifest
-pub fn wit2wadm(resolve: Resolve, world_name: &str) -> anyhow::Result<Manifest> {
-    let wit_parser::World {
-        exports, imports, ..
-    } = resolve
+pub fn wit2wadm_from_folder(
+    wit_folder: impl AsRef<Path>,
+    world_name: impl AsRef<str>,
+) -> anyhow::Result<Manifest> {
+    let mut resolve = Resolve::new();
+
+    resolve
+        .push_path(wit_folder)
+        .context("should be able to load wits")?;
+
+    let world = resolve
         .worlds
         .iter()
-        .find_map(|(_, w)| (w.name == world_name).then_some(w))
+        .find_map(|(_, w)| (w.name == world_name.as_ref()).then_some(w))
+        .cloned()
         .context("component world missing")
         .expect("should be able to find component world");
+
+    let manifest = wit2wadm(resolve, &world).context("should be able to convert to manifest")?;
+
+    Ok(manifest)
+}
+
+/// Loads a WIT component from a file and converts it to a wadm application manifest
+pub fn wit2wadm_from_component(wit_component: impl AsRef<Path>) -> anyhow::Result<Manifest> {
+    let wasm = std::fs::read(&wit_component).context("failed to read WIT component")?;
+    let (resolve, world) =
+        match wit_component::decode(&wasm).context("failed to decode WIT component")? {
+            wit_component::DecodedWasm::Component(resolve, world) => (resolve, world),
+            wit_component::DecodedWasm::WitPackage(..) => {
+                bail!("binary-encoded WIT packages not currently supported")
+            }
+        };
+
+    let world = resolve
+        .worlds
+        .iter()
+        .find_map(|(id, w)| (id == world).then_some(w))
+        .cloned()
+        .context("component world missing")
+        .expect("should be able to find component world");
+
+    let manifest = wit2wadm(resolve, &world).context("should be able to convert to manifest")?;
+
+    Ok(manifest)
+}
+
+/// Converts a component [Resolve] and [World] into a wadm application manifest
+pub fn wit2wadm(resolve: Resolve, world: &World) -> anyhow::Result<Manifest> {
+    let wit_parser::World {
+        exports, imports, ..
+    } = world;
 
     let manifest = manifest::create_manifest(
         // TODO: un-hardcode these values
